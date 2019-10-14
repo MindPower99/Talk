064ded0cdd4b3caa986f55751f1cc00998bcb70d
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-10-14 17:46:37 -0300

    Conexão pronta

--- Talk App/js/login.js
+++ Talk App/js/login.js
@@ -1,5 +1,5 @@
 
-function CriaRequest() {
+/*function CriaRequest() {
      try{
          request = new XMLHttpRequest();        
      }catch (IEAtual){
@@ -84,7 +84,34 @@ $('#btnLogin').click (function () {
                 request.always(function() {
                     console.log("Completo");
                 });
-               
+    });               
     */
-});
+
+    $(document).ready(function(){
+    $('#errolog').hide(); //Esconde o elemento com id errolog
+    $('#formLogin').submit(function(){  //Ao submeter formulário
+        var login=$('#txtEmail').val();    //Pega valor do campo email
+        var senha=$('#txtPass').val();    //Pega valor do campo senha
+
+        console.log(login);
+        console.log(senha);
+        $.ajax({            //Função AJAX
+            url:"http://127.0.0.1/login.php",            //Arquivo php
+            type:"POST",                //Método de envio
+            data: "login="+login+"&senha="+senha,   //Dados
+            success: function (result){         //Sucesso no AJAX
+                        alert(result);
+                        if(result==1){                      
+                            location.href='http://127.0.0.1/restrito.php' 
+                            console.log( "passou");
+                        }else{
+                            $('#errolog').show();   
+                            console.log( "n passou");    //Informa o erro
+                        }
+                    }
+        })
+        return false;   //Evita que a página seja atualizada
+    })
+})
+
 
