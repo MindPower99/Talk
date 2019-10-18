54f45697bdc826c3684cfb97eaba58a88955194e
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-10-17 22:41:03 -0300

    fazendo a listagem do nome do usuario

--- Talk App/php/Listarusuario.php
+++ Talk App/php/Listarusuario.php
@@ -1,3 +1,15 @@
 <?php
 include_once "conexão.php"
+
+$result_usuario = "SELECT * FROM usuario ORDER BY id DESC";
+$resultado_usuario = mysqli_query($conn,$result_usuario);
+
+if(($resultado_usuario) AND ($resultado_usuario->num_rows != 0)) {
+    while($row_usuario = mysqli_fetch_assoc($resultado_usuario)) {
+        echo $row_usuario['nome'] . '<br>';
+    }
+}
+else {
+    
+}
 ?>
