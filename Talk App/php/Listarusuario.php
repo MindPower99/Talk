f6d5fac189987b0a2072893474f4ca8693f52924
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-10-19 20:03:36 -0300

    Listando usuario do banco e arrumando responsividade

--- Talk App/php/Listarusuario.php
+++ Talk App/php/Listarusuario.php
@@ -1,15 +1,16 @@
 <?php
-include_once "conexão.php"
+
+include_once ("conexao.php");
 
 $result_usuario = "SELECT * FROM usuario ORDER BY id DESC";
 $resultado_usuario = mysqli_query($conn,$result_usuario);
 
 if(($resultado_usuario) AND ($resultado_usuario->num_rows != 0)) {
     while($row_usuario = mysqli_fetch_assoc($resultado_usuario)) {
-        echo $row_usuario['nome'] . '<br>';
+        echo $row_usuario['nome'] . "<br>";
     }
 }
 else {
-    
+    echo "Nenhum usuario encontrado";
 }
 ?>
