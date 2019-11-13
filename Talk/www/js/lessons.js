97a19e9faa289ce7dd5e8345b2aecdc7bd703d64
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-11-13 20:43:29 -0300

    função next_content quase completa

--- Talk/www/js/lessons.js
+++ Talk/www/js/lessons.js
@@ -1,5 +1,25 @@
+$(document).ready(function(){
+	$('.c2').hide();
+	$('.c3').hide();
+	
+ });
+ var count = 0;
 function next_content() {
-	console.log("eae");
+	/*console.log("eae");
 	var div = $('.c2');
-	$('.container-fluid').html(div);
+	$('.container-fluid').html(div);*/
+	count++;
+	var a = 1;
+	if (count == a) {
+		var classe = 'c';
+		var classe = 'c' + a;
+		console.log(classe);
+		$('.'+classe).hide();
+		$('.c2').show();
+	}/*
+	var a = $('.c1').attr("value");
+	$('.c1').hide();
+	
+	console.log(a);
+	console.log(count); */
 }
