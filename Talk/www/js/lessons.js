4b9cda457ba9bcf9e101e8c994d1350d4a868afd
MindPower99 <gabriel-jorge1999@hotmail.com>
2019-11-21 04:26:42 -0200

    arrumando a função next_content/completo

--- Talk/www/js/lessons.js
+++ Talk/www/js/lessons.js
@@ -1,21 +1,32 @@
 $(document).ready(function(){
 	$('.c2').hide();
 	$('.c3').hide();
+	$('.c4').hide();
+	$('.c5').hide();
 	
  });
  var count = 0;
+ var a = 1;
 function next_content() {
 	/*console.log("eae");
 	var div = $('.c2');
 	$('.container-fluid').html(div);*/
 	count++;
-	var a = 1;
-	if (count == a) {
 		var classe = 'c';
 		var classe = 'c' + a;
 		console.log(classe);
 		$('.'+classe).hide();
-		$('.c2').show();
+		a = a + 1;
+		var classe =  'c' + a;
+		$('.'+classe).show();
+		console.log(classe);
+		/*if (count > 1) {
+			$('.'+classe).hide();
+			a = a + 1;
+			var classe =  'c' + a;
+			$('.'+classe).show();
+			console.log(classe);
+	
 	}/*
 	var a = $('.c1').attr("value");
 	$('.c1').hide();
